6ad35ba7aee9aa20494dda9cc357b477f208532a
benoit moret <benoit.moret@gmail.com>
2022-06-01 21:48:47 +0200

    searchmoviesV1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events, only:[:index, :show, :update, :new, :create, :destroy] do
-    
-  end
+  resources :events, only:[:index, :show, :update, :new, :create, :destroy] 
 end
