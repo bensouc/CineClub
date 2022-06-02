fb7e8e399b73b895085ae2b56546800b03f09e81
benoit moret <benoit.moret@gmail.com>
2022-06-02 10:04:42 +0200

    update movie model for tmdb management

--- app/javascript/controllers/movie_search_controller.js
+++ app/javascript/controllers/movie_search_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["query","results"]
+  static targets = ["query", "results"]
 
   connect() {
     console.log('results conntecté')
@@ -12,23 +12,46 @@ export default class extends Controller {
     this.resultsTarget.innerHTML = ""
     // console.log(this.queryTarget.value)
     const query = this.queryTarget.value
-
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=5d25d045ccb74424de93b9f3878f1b6c&query=${query}&language=fr`)
-    .then(response => response.json())
-    .then(data => this.insertMovies(data))
+    if (query == '' || query == 'Titre') {
+      const movieTag =
+        `<div class="noresult">
+          <h5>Le champ doit<br> être rempli</h5>
+        </div>`
+      this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
+    }
+else
+    {fetch(`https://api.themoviedb.org/3/search/movie?api_key=5d25d045ccb74424de93b9f3878f1b6c&query=${query}&language=fr`)
+      .then(response => response.json())
+      .then(data => this.insertMovies(data))}
   }
 
   insertMovies(data) {
-    data.results.forEach((result) => {
+    if (data.total_results == 0) {
       const movieTag =
-      `<li class="mb-3">
-      <img src="https://image.tmdb.org/t/p/w300${result.poster_path}" alt="" width="230" data-action ='click->movie-search#add_movie'>
-      </li>`
+        `<div class="noresult">
+          <h5>Aucun résultat <br> correspondant</h5>
+        </div>`
       this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
-    })
+    }
+    else {
+      data.results.forEach((result) => {
+        const movieTag =
+          `<li class="m-3">
+              <img src="https://image.tmdb.org/t/p/w300${result.poster_path}" alt="" width="230" data-action ='click->movie-search#add_movie'
+              info={  title: ${result.title},
+                      poster_url: https://image.tmdb.org/t/p/w300${result.poster_path},
+                      trailer_url:,
+                      year: ${result.release_date},
+                      tmdb_id: ${result.id},
+                      tmdb_genre_id : ${result.genre_ids}
+                    }>
+          </li>`
+        this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
+      })
+    }
   }
 
-  add_movie(event){
+  add_movie(event) {
     console.log("add movie")
   }
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,9 @@
 class Movie < ApplicationRecord
   has_many :choices, dependent: :destroy
 
-  
-
   validates :title, presence: true
-  validates :kind, presence: true
+  validates :kind
   validates :poster_url, presence: true
-  validates :trailer_url, presence: true
+  validates :trailer_url
+  validates :tmdb_id, uniqueness: true
 end
