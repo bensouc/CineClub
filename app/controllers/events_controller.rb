a1db9d32659d3866ac0b5f6d06750504bcc45a90
benoit moret <benoit.moret@gmail.com>
2022-06-03 10:51:23 +0200

    order event desc

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order(date: :desc)
   end
   def show
     @event = Event.find(params[:id])
