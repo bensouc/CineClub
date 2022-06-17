9b4c05f54227eb8da1fae3c26285b231c696ca22
benoit moret <benoit.moret@gmail.com>
2022-06-17 13:34:02 +0200

    manage execo display in events index & favicon

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
   has_many :choices
   has_many :movies, through: :choices
-  
+
+  def exequo?
+    choices = self.choices.order(ranking: :desc)
+    return false if choices.count < 2
+
+    return true if choices.first.ranking == choices[1].ranking
+
+    return false
+  end
 end
